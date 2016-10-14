dfe31a17b894907108875f3e68d52d03e872f461
Almaz Vagapov <almaznv@gmail.com>
2016-10-14 15:13:21 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/assets/css/**/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/assets/css/main.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/build/assets/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
